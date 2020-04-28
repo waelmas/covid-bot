2e7c862bc855f69f9ead09ad0d15cc096f6e0b5c
Wael Al Masri <43114112+waelmas@users.noreply.github.com>
2020-04-29 00:19:19 +0200

    Update covid-embed.js

--- static/covid-embed.js
+++ static/covid-embed.js
@@ -12,8 +12,8 @@
         document.getElementById("covid-bot-outer").style.display="unset";
         document.getElementById("covid-chat").style.display="none";
       var opened= document.getElementById("covid-frame").src;
-      if (opened != "https://covid-bot.com/"){
-        document.getElementById("covid-frame").src="https://covid-bot.com";
+      if (opened != "https://covid-bot.com/chat/"){
+        document.getElementById("covid-frame").src="https://covid-bot.com/chat/";
       }else{}
 
       var bodyofhost = document.getElementsByTagName("BODY")[0];
